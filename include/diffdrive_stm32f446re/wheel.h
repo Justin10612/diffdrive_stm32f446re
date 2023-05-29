954d4d34993e635bbce12a672a605081d9a06528
Justin10612 <atom.9031@gmail.com>
2023-05-29 10:29:55 +0800

    adding annotation

--- include/diffdrive_stm32f446re/wheel.h
+++ include/diffdrive_stm32f446re/wheel.h
@@ -8,13 +8,13 @@ class Wheel
 {
     public:
 
-    std::string name = "";
-    int enc = 0;
-    double cmd = 0;
-    double pos = 0;
-    double vel = 0;
-    double eff = 0;
-    double velSetPt = 0;
+    std::string name = "";  // wheel name
+    int enc = 0;    // encoder counts
+    double cmd = 0; // command velocity
+    double pos = 0; // feedback position
+    double vel = 0; // feedback velocity
+    // double eff = 0;
+    // double velSetPt = 0;
     double rads_per_count = 0;
 
     Wheel() = default;
