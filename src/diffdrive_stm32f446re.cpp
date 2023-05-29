33cc992ea81258f0a299d71532331a6eb3f248c2
Justin10612 <atom.9031@gmail.com>
2023-05-29 11:00:28 +0800

    deal with interface

--- src/diffdrive_stm32f446re.cpp
+++ src/diffdrive_stm32f446re.cpp
@@ -101,25 +101,28 @@ hardware_interface::return_type DiffBotSystemHardware::configure(
 std::vector<hardware_interface::StateInterface> DiffBotSystemHardware::export_state_interfaces()
 {
   std::vector<hardware_interface::StateInterface> state_interfaces;
-  // for (auto i = 0u; i < info_.joints.size(); i++)
-  // {
-  //   state_interfaces.emplace_back(hardware_interface::StateInterface(
-  //     info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_positions_[i]));
-  //   state_interfaces.emplace_back(hardware_interface::StateInterface(
-  //     info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_velocities_[i]));
-  // }
+  // wheel state interface:pos
+  state_interfaces.emplace_back(hardware_interface::StateInterface(
+    wheel_l_.name, hardware_interface::HW_IF_POSITION, &wheel_l_.pos));
+  state_interfaces.emplace_back(hardware_interface::StateInterface(
+    wheel_r_.name, hardware_interface::HW_IF_POSITION, &wheel_r_.pos));
 
+  // state interface:wheel_vel
+  state_interfaces.emplace_back(hardware_interface::StateInterface(
+    wheel_l_.name, hardware_interface::HW_IF_VELOCITY, &wheel_l_.vel));
+  state_interfaces.emplace_back(hardware_interface::StateInterface(
+    wheel_r_.name, hardware_interface::HW_IF_VELOCITY, &wheel_r_.vel));
   return state_interfaces;
 }
 
 std::vector<hardware_interface::CommandInterface> DiffBotSystemHardware::export_command_interfaces()
 {
   std::vector<hardware_interface::CommandInterface> command_interfaces;
-  // for (auto i = 0u; i < info_.joints.size(); i++)
-  // {
-  //   command_interfaces.emplace_back(hardware_interface::CommandInterface(
-  //     info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_commands_[i]));
-  // }
+  // command interface:wheel cmd_vel
+  command_interfaces.emplace_back(hardware_interface::CommandInterface(
+    wheel_l_.name, hardware_interface::HW_IF_VELOCITY, &wheel_l_.cmd));
+  command_interfaces.emplace_back(hardware_interface::CommandInterface(
+    wheel_r_.name, hardware_interface::HW_IF_VELOCITY, &wheel_r_.cmd));
 
   return command_interfaces;
 }
