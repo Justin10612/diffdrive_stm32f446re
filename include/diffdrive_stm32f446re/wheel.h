57d1d308226fad804af9550ab361844ee2bd97eb
Justin10612 <atom.9031@gmail.com>
2023-05-29 10:27:42 +0800

    Create a file about encoder cal

---
+++ include/diffdrive_stm32f446re/wheel.h
@@ -0,0 +1,40 @@
+#ifndef DIFFDRIVE_ARDUINO_WHEEL_H
+#define DIFFDRIVE_ARDUINO_WHEEL_H
+
+#include <string>
+#include <cmath>
+
+class Wheel
+{
+    public:
+
+    std::string name = "";
+    int enc = 0;
+    double cmd = 0;
+    double pos = 0;
+    double vel = 0;
+    double eff = 0;
+    double velSetPt = 0;
+    double rads_per_count = 0;
+
+    Wheel() = default;
+
+    Wheel(const std::string &wheel_name, int counts_per_rev)
+    {
+        setup(wheel_name, counts_per_rev);
+    }
+    
+    void setup(const std::string &wheel_name, int counts_per_rev){
+        name = wheel_name;
+        rads_per_count = (2*M_PI)/counts_per_rev;
+    }
+
+    double calcEncAngle(){
+        return enc*rads_per_count;
+    }
+
+
+};
+
+
+#endif // DIFFDRIVE_ARDUINO_WHEEL_H
